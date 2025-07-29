48dd8be4e8bbfa6a40e255878932045d2dd14a17
Tuntufye Mwakalasya <87774338+Tmwakalasya@users.noreply.github.com>
2025-07-29 15:20:09 -0400

    Redesigned API headers and Sentiment Pipeiline

--- reddit_sentiment_pipeline/src/api/routes.py
+++ reddit_sentiment_pipeline/src/api/routes.py
@@ -1,20 +1,42 @@
 from flask import Flask, request, jsonify
-from transformers import BertTokenizer
-from reddit_sentiment_pipeline.data.raw.reddit_fetcher import clean_text_for_bert
-app = Flask(__name__)
+import pandas as pd
 import datetime
+import os
+app = Flask(__name__)
+
+def load_file(filepath: str):
+    try:
+        if os.path.exists(filepath):
+            print("Loading the file.....")
+            df = pd.read_parquet(filepath)
+            print(f"Length of dataframe: {len(df)}")
+            return df
+        else:
+            print("File not found...")
+            return None
+    except Exception as e:
+        print(f"Failed to load file: {e}")
+        return None
+
 
 @app.route('/')
 def welcome():
     print(f"{datetime.datetime.now()}")
     return f'Welcome to the Reddit Sentiment Pipeline'
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/summary',methods=['GET'])
+def get_summary():
     pass
 
-
-
-
+@app.route('/comments',methods=['GET'])
+def get_comments():
+    pass
+@app.route('/positive',methods=['GET'])
+def positive_comments():
+    pass
+@app.route('/negative',methods=['GET'])
+def negative_comments():
+    pass
 
 if __name__ == '__main__':
+    load_file("final_cleaned_data.parquet")
     app.run(debug=True)
